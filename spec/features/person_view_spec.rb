8fa3e29ef524ba86abad5cd575f68ff803bc0d8c
Andrew Watkins <andrew.watkinz@gmail.com>
2014-08-05 19:46:32 -0600

    fixes add phone number path

--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -1,21 +1,31 @@
-require 'spec_helper'
-require 'capybara/rails'
-require 'capybara/rspec'
+require 'rails_helper'
 
 describe 'the person view', type: :feature do
 
-  let(:person) { Person.create(first_name: 'Schooby', last_name: 'The Dog') }
+  let(:person) { Person.create(first_name: 'John', last_name: 'Doe') }
 
   before(:each) do
     person.phone_numbers.create(number: '555-1111')
     person.phone_numbers.create(number: '555-2222')
+    visit person_path(person)
+  end
+
+  it 'has a link to add a new phone number' do
+    expect(page).to have_link('Add phone number', href: new_phone_number_path(person_id: person.id))
   end
 
   it 'shows the phone numbers' do
-    pending
     person.phone_numbers.each do |phone|
       expect(page).to have_content(phone.number)
     end
+  end
+
+  it 'adds a new phone number' do
+    page.click_link('Add phone number')
+    page.fill_in('Number', with: '555-8888')
+    page.click_button('Create Phone number')
+    expect(current_path).to eq(person_path(person))
+    expect(page).to have_content('555-8888')
   end
 
 end
