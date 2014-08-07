d6da480337e96091cb56c5e86c303055aea5b5ee
Andrew Watkins <andrew.watkinz@gmail.com>
2014-08-07 09:50:16 -0600

    adds update and add email address features

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -11,9 +11,6 @@ RSpec.describe PhoneNumbersController, :type => :controller do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # PhoneNumbersController. Be sure to keep this updated too.
   let(:valid_session) { {} }
 
   describe "GET index" do
